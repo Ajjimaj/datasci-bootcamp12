82762616b04d5c28c06b093912f5101516f44fb6
Ajjima <ajjimajom@gmail.com>
2026-04-25 14:50:42 +0700

    replace old message

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
 ## print hello world
-print("Hello World")
-
-## print Good Bye
-print("Good Bye")
-
-## print learning
-print("I'm Learning Github")
-
-## new function
-def add_two(num):
-   return num + 2
-
-print(add_two(2), add_two(5))
+print("Now I know what git and git hub is.")
